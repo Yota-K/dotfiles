1a058c4e6e762de0a6f5e0f3bf1863502f737ace
紅梅陽太 <powdersugar828828@gmail.com>
2021-04-20 22:56:45 +0900

    シンボリックリンクを設定するコマンド追加。

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 path=~/dotfiles
 
 if [ $path ]; then
+  ln -sf $PWD/.agignore ~/.agignore
+  ln -sf $PWD/.bash_profile ~/.bash_profile
+  ln -sf $PWD/.bashrc ~/.bashrc
+  ln -sf $PWD/.tigrc ~/.tigrc
+  ln -sf $PWD/.tmux.conf ~/.tmux.conf
+  ln -sf $PWD/.vimrc ~/.vimrc
   ln -sf $PWD/Brewfile ~/Brewfile
 fi
