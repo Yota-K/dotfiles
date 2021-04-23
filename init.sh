41828b81c41ad0afce04bba5bb07e058adc335ee
紅梅陽太 <powdersugar828828@gmail.com>
2021-04-23 23:02:25 +0900

    iterm2のテーマにシンボリックリンクを貼る処理を追加。

--- init.sh
+++ init.sh
@@ -1,11 +1,31 @@
+# -e・・・エラーが処理を中断
+# -u・・・未定義の変数を使おうとすると処理中断
+#!/bin/bash -eu
+
 path=~/dotfiles
+root_path=~/
+
+ln -sf $PWD/.agignore ~/.agignore
+ln -sf $PWD/.bash_profile ~/.bash_profile
+ln -sf $PWD/.bashrc ~/.bashrc
+ln -sf $PWD/.tigrc ~/.tigrc
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/Brewfile ~/Brewfile
+
+cd $root_path
+
+if [ ! -e ~/iterm2 ]; then
+  mkdir -p ~/iterm2
+fi
+
+if [ ! -e ~/.vim ]; then
+  mkdir -p ~/.vim
+fi
+
+# ~/dotfilesに戻る
+cd $path
 
 if [ $path ]; then
-  ln -sf $PWD/.agignore ~/.agignore
-  ln -sf $PWD/.bash_profile ~/.bash_profile
-  ln -sf $PWD/.bashrc ~/.bashrc
-  ln -sf $PWD/.tigrc ~/.tigrc
-  ln -sf $PWD/.tmux.conf ~/.tmux.conf
-  ln -sf $PWD/.vimrc ~/.vimrc
-  ln -sf $PWD/Brewfile ~/Brewfile
+  ln -sf $PWD/iterm2/tron.itermcolors ~/iterm2/
 fi
