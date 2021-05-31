abe75a0286c7b04121cb2d91470573832885137e
紅梅陽太 <powdersugar828828@gmail.com>
2021-05-31 23:32:56 +0900

    Vueのテンプレの修正。

--- .vim/templates/vue/base-vue-script.vue
+++ .vim/templates/vue/base-vue-script.vue
@@ -2,9 +2,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  setup() {}
-})
+  setup() {
+    return {
+
+    };
+  },
+});
 </script>
