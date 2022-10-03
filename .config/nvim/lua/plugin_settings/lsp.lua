938fcb713dc2bdd0a3cbb95b8b2ece03ce7fccf8
Y.K <powdersugar828828@gmail.com>
2022-10-04 00:07:45 +0900

    [nvim] lspのハイライトする処理消して。

--- .config/nvim/lua/plugin_settings/lsp.lua
+++ .config/nvim/lua/plugin_settings/lsp.lua
@@ -35,15 +35,10 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 )
 -- Reference highlight
 vim.cmd [[
-set updatetime=500
-highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-augroup lsp_document_highlight
-  autocmd!
-  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-augroup END
+  set updatetime=500
+  highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
 ]]
 
 -- lsp settings override
@@ -83,3 +78,7 @@ cmp.setup({
     ghost_text = true,
   },
 })
+
+vim.cmd[[
+  set completeopt=menuone,noinsert,noselect
+]]
