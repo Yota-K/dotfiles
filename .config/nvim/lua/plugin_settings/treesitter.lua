d87fd71d2286994c1b90f1d7e69af8766cc522f4
Y.K <powdersugar828828@gmail.com>
2022-10-08 20:23:54 +0900

    [nvim] Added parser of treesitter.

--- .config/nvim/lua/plugin_settings/treesitter.lua
+++ .config/nvim/lua/plugin_settings/treesitter.lua
@@ -1,7 +1,21 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'css', 'fish', 'go', 'html', 'javascript', 'scss', 'php', 'tsx', 'vim', 'vue', 'yaml' },
+  ensure_installed = {
+    'css',
+    'fish',
+    'go',
+    'html',
+    'javascript',
+    'json',
+    'lua',
+    'php',
+    'scss',
+    'tsx',
+    'typescript',
+    'vim',
+    'vue',
+    'yaml'
+  },
   highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = { 'ruby' },  -- list of language that will be disabled
+    enable = true,
   },
 }
