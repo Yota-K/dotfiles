7f7348dd51ead001849713c7a43939a127facade
Y.K <powdersugar828828@gmail.com>
2022-10-10 00:32:09 +0900

    [nvim] Fixed indent.

--- .config/nvim/lua/plugin_settings/lsp.lua
+++ .config/nvim/lua/plugin_settings/lsp.lua
@@ -1,20 +1,20 @@
 vim.cmd[[
-" MEMO
-" control + n 下に移動
-" control + p 上に移動
-"
-" ヒントを表示
-" silent・・・コマンドラインへの出力を抑制する
-" <C-u>hoge<cr>・・・特殊なキーのマッピング
-" https://thinca.hatenablog.com/entry/20100205/1265307642
-nnoremap <silent> <space>h :<C-u>call CocAction('doHover')<cr>
+  " MEMO
+  " control + n 下に移動
+  " control + p 上に移動
+  "
+  " ヒントを表示
+  " silent・・・コマンドラインへの出力を抑制する
+  " <C-u>hoge<cr>・・・特殊なキーのマッピング
+  " https://thinca.hatenablog.com/entry/20100205/1265307642
+  nnoremap <silent> <space>h :<C-u>call CocAction('doHover')<cr>
 
-" cocで表示されるフローティングウィンドウのスクロールのキーマッピング
-" issues: https://github.com/neoclide/coc.nvim/issues/609
-nnoremap <nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-nnoremap <nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-inoremap <nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
-inoremap <nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+  " cocで表示されるフローティングウィンドウのスクロールのキーマッピング
+  " issues: https://github.com/neoclide/coc.nvim/issues/609
+  nnoremap <nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+  nnoremap <nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+  inoremap <nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+  inoremap <nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
 
-nnoremap <silent> <space>l :CocList<CR>
+  nnoremap <silent> <space>l :CocList<CR>
 ]]
