35170273c02760f901a08dace02cffd8d1b5afd3
Y.K <powdersugar828828@gmail.com>
2022-10-12 01:14:29 +0900

    [tig・fish] Adjustment tig and fish colors.

--- fish/config.fish
+++ fish/config.fish
@@ -88,6 +88,30 @@ alias tree="tree -I node_modules -L 3"
 alias dotfiles="cd ~/dotfiles && v"
 
 #########################################
+# CLIの色の変更
+#########################################
+
+set fish_color_normal         brwhite
+set fish_color_autosuggestion brblack
+set fish_color_cancel         brcyan
+set fish_color_command        brpurple
+set fish_color_comment        brblack
+set fish_color_cwd            brred
+set fish_color_end            brwhite
+set fish_color_error          brred
+set fish_color_escape         brcyan
+set fish_color_host           brgreen
+set fish_color_host_remote    bryellow
+set fish_color_match          brcyan --underline
+set fish_color_operator       brpurple
+set fish_color_param          brred
+set fish_color_quote          brgreen
+set fish_color_redirection    brcyan
+set fish_color_search_match   --background=brblack
+set fish_color_selection      --background=brblack
+set fish_color_user           brblue
+
+#########################################
 # 関数
 #########################################
 
