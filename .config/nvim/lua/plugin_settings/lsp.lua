e2199891f404891f38a7f708b2b32f8cfb298861
Y.K <powdersugar828828@gmail.com>
2022-10-15 23:17:25 +0900

    [nvim] coc.nvim settings replace to lua.

--- .config/nvim/lua/plugin_settings/lsp.lua
+++ .config/nvim/lua/plugin_settings/lsp.lua
@@ -1,20 +1,29 @@
-vim.cmd[[
-  " MEMO
-  " control + n 下に移動
-  " control + p 上に移動
-  "
-  " ヒントを表示
-  " silent・・・コマンドラインへの出力を抑制する
-  " <C-u>hoge<cr>・・・特殊なキーのマッピング
-  " https://thinca.hatenablog.com/entry/20100205/1265307642
-  nnoremap <silent> <space>h :<C-u>call CocAction('doHover')<cr>
+-- NOTE
+--
+-- 補完の移動
+-- control + n 下に移動
+-- control + p 上に移動
+-- 
+-- silent・・・コマンドラインへの出力を抑制する
+-- <C-u>hoge<cr>・・・特殊なキーのマッピング
+-- https://thinca.hatenablog.com/entry/20100205/1265307642
+local keyset = vim.api.nvim_set_keymap
 
-  " cocで表示されるフローティングウィンドウのスクロールのキーマッピング
-  " issues: https://github.com/neoclide/coc.nvim/issues/609
-  nnoremap <nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
-  nnoremap <nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
-  inoremap <nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
-  inoremap <nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+-- ヒントを表示
+keyset('n', '<space>h', ':<C-u>call CocAction("doHover")<cr>', {
+  silent = true
+})
 
-  nnoremap <silent> <space>l :CocList<CR>
-]]
+-- CocListを表示
+keyset('n', '<space>l', ':CocList<cr>', {
+  silent = true
+})
+
+-- cocで表示されるフローティングウィンドウのスクロールのキーマッピング
+-- issues: https://github.com/neoclide/coc.nvim/issues/609
+local opts = { silent = true, nowait = true, expr = true }
+
+keyset('n', '<C-f>', 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
+keyset('n', '<C-b>', 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
+keyset('i', '<C-f>', 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"', opts)
+keyset('i', '<C-b>', 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"', opts)
