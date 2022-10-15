ba6a13c0dee48695587b70afb5358448c66d8c7c
Y.K <powdersugar828828@gmail.com>
2022-10-15 23:19:24 +0900

    [nvim] Added packer error handling.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,9 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print('Packer is not installed')
+  return
+end
+
 -- this file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- only required if you have packer configured as `opt`
@@ -67,5 +73,5 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
   -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
-}
+  }
 end)
