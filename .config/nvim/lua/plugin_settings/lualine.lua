11b9cc4c191fbc9fde0dcf67e9540c7d79c2182b
Y.K <powdersugar828828@gmail.com>
2022-10-18 21:09:30 +0900

    [nvim] Change lualine color.

--- .config/nvim/lua/plugin_settings/lualine.lua
+++ .config/nvim/lua/plugin_settings/lualine.lua
@@ -1,10 +1,28 @@
 local status, lualine = pcall(require, 'lualine')
 if (not status) then return end
 
+local cusutom_nightfly = require('lualine.themes.nightfly')
+
+local base_colors = {
+  black = '#1c1c1c',
+  text_color = '#fffff'
+}
+
+-- normal mode
+cusutom_nightfly.normal.a.bg = base_colors['black']
+cusutom_nightfly.normal.a.fg = 'white'
+cusutom_nightfly.normal.b.bg = base_colors['black']
+cusutom_nightfly.normal.c.bg = base_colors['black']
+
+-- default
+cusutom_nightfly.inactive.a.bg = base_colors['black']
+cusutom_nightfly.inactive.b.bg = base_colors['black']
+cusutom_nightfly.inactive.c.bg = base_colors['black']
+
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'nightfly',
+    theme = cusutom_nightfly,
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = ''},
     disabled_filetypes = {}
