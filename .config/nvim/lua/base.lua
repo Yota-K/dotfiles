bc0a2e3a150119c99ce70a673da0080d022720f8
Y.K <powdersugar828828@gmail.com>
2022-10-27 00:58:56 +0900

    [nvim] Refactor nvim base settings.

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,52 +1,54 @@
---  __/\\\________/\\\__/\\\\\\\\\\\__/\\\\____________/\\\\_        
---   _\/\\\_______\/\\\_\/////\\\///__\/\\\\\\________/\\\\\\_       
---    _\//\\\______/\\\______\/\\\_____\/\\\//\\\____/\\\//\\\_      
---     __\//\\\____/\\\_______\/\\\_____\/\\\\///\\\/\\\/_\/\\\_     
---      ___\//\\\__/\\\________\/\\\_____\/\\\__\///\\\/___\/\\\_    
---       ____\//\\\/\\\_________\/\\\_____\/\\\____\///_____\/\\\_   
---        _____\//\\\\\__________\/\\\_____\/\\\_____________\/\\\_  
---         ______\//\\\________/\\\\\\\\\\\_\/\\\_____________\/\\\_ 
---          _______\///________\///////////__\///______________\///__
--- 
-vim.opt.encoding = 'utf-8'
+--
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
 
-vim.opt.helplang = 'ja,en'
-vim.opt.numberwidth = 6
+local opt = vim.opt
 
-vim.opt.ruler = true
+opt.encoding = 'utf-8'
 
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+opt.helplang = 'ja,en'
+opt.numberwidth = 6
 
-vim.opt.showcmd = true
+opt.ruler = true
 
-vim.opt.laststatus = 2
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
 
-vim.opt.showmatch.matchtime = 1
+opt.showcmd = true
 
-vim.opt.backspace = 'indent,eol,start'
+opt.laststatus = 2
 
-vim.opt.virtualedit = 'onemore'
+opt.showmatch.matchtime = 1
 
-vim.opt.swapfile = false
+opt.backspace = 'indent,eol,start'
 
-vim.opt.clipboard = 'unnamedplus'
+opt.virtualedit = 'onemore'
 
-vim.opt.wildmenu = true
-vim.opt.history = 5000
+opt.swapfile = false
 
-vim.opt.wrap = false
+opt.clipboard = 'unnamedplus'
 
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.wildmenu = true
+opt.history = 5000
+
+opt.wrap = false
+
+opt.incsearch = true
+opt.hlsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
 -- Global Status Line を有効にする
 -- 画面をスプリットした時に Status LineはSplitされないようにする
-vim.opt.laststatus = 3
+opt.laststatus = 3
 
 vim.cmd [[
   augroup highlightIdegraphicSpace
