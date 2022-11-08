20f16c61fcc9d6e09e67609eedab59a4306c3695
Y.K <powdersugar828828@gmail.com>
2022-11-09 01:39:35 +0900

    [nvim] Refactor lsp settings.

--- .config/nvim/lua/plugin_settings/lsp.lua
+++ .config/nvim/lua/plugin_settings/lsp.lua
@@ -8,25 +8,23 @@
 -- <C-u>hoge<cr>・・・特殊なキーのマッピング
 -- https://thinca.hatenablog.com/entry/20100205/1265307642
 local keyset = vim.api.nvim_set_keymap
+local opts = {
+  default = { silent = true, nowait = true, expr = true },
+  silent_only =  { silent = true }
+}
 
 -- ヒントを表示
-keyset('n', '<space>h', ':<C-u>call CocAction("doHover")<cr>', {
-  silent = true
-})
+keyset('n', '<space>h', ':<C-u>call CocAction("doHover")<cr>', opts.silent_only)
 
 -- CocListを表示
-keyset('n', '<space>l', ':CocList<cr>', {
-  silent = true
-})
+keyset('n', '<space>l', ':CocList<cr>', opts.silent_only)
 
 -- 定義元ジャンプ
-keyset('n', '<space>]', '<Plug>(coc-definition)', { silent = true })
+keyset('n', '<space>]', '<Plug>(coc-definition)', opts.silent_only)
 
 -- cocで表示されるフローティングウィンドウのスクロールのキーマッピング
 -- issues: https://github.com/neoclide/coc.nvim/issues/609
-local opts = { silent = true, nowait = true, expr = true }
-
-keyset('n', '<C-f>', 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
-keyset('n', '<C-b>', 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
-keyset('i', '<C-f>', 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"', opts)
-keyset('i', '<C-b>', 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"', opts)
+keyset('n', '<C-f>', 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts.default)
+keyset('n', '<C-b>', 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts.default)
+keyset('i', '<C-f>', 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"', opts.default)
+keyset('i', '<C-b>', 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"', opts.default)
