5aa2bbb4c4088995bd5eb833e59a24d2c96966cc
Y.K <powdersugar828828@gmail.com>
2023-03-31 21:40:55 +0900

    [nvim] Undid filer width.

--- .config/nvim/lua/plugin_settings/fern.lua
+++ .config/nvim/lua/plugin_settings/fern.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':Fern . -reveal=% -drawer -toggle<CR>', {
 vim.cmd([[
   let g:fern#renderer = 'devicons'
   let g:fern#default_hidden = 1
-  let g:fern#drawer_width = 50
+  let g:fern#drawer_width = 40
   let g:fern_disable_startup_warnings = 1
 
   function! s:init_fern() abort
