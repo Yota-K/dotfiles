6eefc5ceba3ffe7e04fb0c6e08913ebf677692b6
Y.K <powdersugar828828@gmail.com>
2023-07-03 00:08:37 +0900

    [nvim] Added code that ignored standard neovim plugins.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,11 +13,6 @@ require ('functions')
 require ('keymap')
 require ('plugins')
 require ('terminal')
-
--- PackerCompileを自動実行
-vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
-
--- nightfox theme Default options
 require('nightfox').setup({
   groups = {
     nightfox = {
@@ -41,6 +36,21 @@ require('nightfox').setup({
     },
   },
 })
+
+-- 不要なデフォルトプラグインを無効化する
+-- 参考: https://zenn.dev/kawarimidoll/articles/8172a4c29a6653
+vim.api.nvim_set_var('did_install_default_menus', 1)
+vim.api.nvim_set_var('did_install_syntax_menu', 1)
+vim.api.nvim_set_var('did_indent_on', 1)
+vim.api.nvim_set_var('loaded_man', 1)
+vim.api.nvim_set_var('loaded_matchit', 1)
+vim.api.nvim_set_var('loaded_matchparen', 1)
+vim.api.nvim_set_var('loaded_tarPlugin', 1)
+vim.api.nvim_set_var('loaded_tutor_mode_plugin', 1)
+vim.api.nvim_set_var('skip_loading_mswin', 1)
+
+-- PackerCompileを自動実行
+vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
 
 -- setup must be called before loading
 -- vim.cmd("colorscheme nightfox")
