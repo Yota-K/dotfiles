9893e262cd0644f6c86ccd31f8ee24f27268440b
Y.K <powdersugar828828@gmail.com>
2023-08-03 01:03:57 +0900

    [nvim] Added keymap for nvim lsp that show error.

--- .config/nvim/lua/plugin_settings/lsp.lua
+++ .config/nvim/lua/plugin_settings/lsp.lua
@@ -66,6 +66,8 @@ vim.keymap.set('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<space>]', '<cmd>lua vim.lsp.buf.definition()<CR>')
 -- カーソル下の変数をコード内で参照している箇所を一覧表示
 vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+-- エラーを表示
+vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
 
 -- 必要になったらコメントアウト解除して使えるようにする
 -- vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
@@ -74,7 +76,6 @@ vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
 -- vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
 -- vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
 -- vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
--- vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
 -- vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
 -- vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 
