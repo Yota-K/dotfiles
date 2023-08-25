7092c5d079ff45623a234032613bee4f7419b602
Y.K <powdersugar828828@gmail.com>
2023-08-25 17:40:06 +0900

    [nvim] Refactored init.lua of plugin_settings dir.

--- .config/nvim/lua/plugin_settings/init.lua
+++ .config/nvim/lua/plugin_settings/init.lua
@@ -1,14 +1,14 @@
 return {
-  plugin1 = require('plugin_settings.ale'),
-  plugin2 = require('plugin_settings.any-jump'),
-  plugin3 = require('plugin_settings.comment-out'),
-  plugin4 = require('plugin_settings.emmet'),
-  plugin5 = require('plugin_settings.dial'),
-  plugin6 = require('plugin_settings.fern'),
-  plugin7 = require('plugin_settings.git-nvim'),
-  plugin8 = require('plugin_settings.lsp'),
-  plugin9 = require('plugin_settings.lualine'),
-  plugin10 = require('plugin_settings.treesitter'),
-  plugin11 = require('plugin_settings.telescope'),
-  plugin12 = require('plugin_settings.vim-findroot')
+  require('plugin_settings.ale'),
+  require('plugin_settings.any-jump'),
+  require('plugin_settings.comment-out'),
+  require('plugin_settings.emmet'),
+  require('plugin_settings.dial'),
+  require('plugin_settings.fern'),
+  require('plugin_settings.git-nvim'),
+  require('plugin_settings.lsp'),
+  require('plugin_settings.lualine'),
+  require('plugin_settings.treesitter'),
+  require('plugin_settings.telescope'),
+  require('plugin_settings.vim-findroot')
 }
