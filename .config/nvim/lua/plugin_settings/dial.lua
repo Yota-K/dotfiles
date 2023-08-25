657ec9f57ca0ba337154596305e633349fc88891
Y.K <powdersugar828828@gmail.com>
2023-08-25 17:32:39 +0900

    [nvim] Added toggle text format settings for dial.nvim.

--- .config/nvim/lua/plugin_settings/dial.lua
+++ .config/nvim/lua/plugin_settings/dial.lua
@@ -12,8 +12,11 @@ vim.keymap.set('v', 'g<C-x>', dial_map.dec_gvisual(), {noremap = true })
 local augend = require('dial.augend')
 
 require('dial.config').augends:register_group{
-  -- default augends used when no group name is specified
   default = {
+    -- toggle text format types
+    augend.case.new({
+      types = { 'camelCase', 'snake_case', 'kebab-case', 'SCREAMING_SNAKE_CASE', 'PascalCase' },
+    }),
     augend.integer.alias.decimal,    -- nonnegative decimal number (0, 1, 2, 3, ...)
     augend.integer.alias.hex,        -- nonnegative hex number  (0x01, 0x1a1f, etc.)
     augend.date.alias['%Y/%m/%d'],   -- date (2022/02/19, etc.)
