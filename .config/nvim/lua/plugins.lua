415a9979bb4045fbfe15d4674bc936c43046ec63
Y.K <powdersugar828828@gmail.com>
2023-10-19 02:56:13 +0900

    [nvim] Performance tuning of nvim.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -7,135 +7,167 @@
 -- /usr/local/lib/lua/5.4/?/init.lua;
 -- ./?.lua;
 -- ./?/init.lua
-local module_path = ';'..os.getenv('HOME')..'/dotfiles/?.lua;'
-package.path = package.path..module_path
-local theme = require('theme')
+local module_path = ";" .. os.getenv("HOME") .. "/dotfiles/?.lua;"
+package.path = package.path .. module_path
+local theme = require("theme")
 
 -- Install and configure lazy.nvim if not already installed
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  endvim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
+	endvim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Initialize lazy.nvim
-require('lazy')
+require("lazy")
 
 -- Set mapleader
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Load plugins using lazy.nvim syntax
-require('lazy').setup({
-  -- メインで使用してるテーマ
-  {
-    'EdenEast/nightfox.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd('colorscheme nightfox')
-      -- vim.cmd('colorscheme carbonfox')
-    end,
-    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
-    init = function()
-      require('nightfox').setup({
-        groups = {
-          nightfox = theme.theme_override_settings('nightfox'),
-          carbonfox = theme.theme_override_settings('carbonfox'),
-        },
-      })
-    end,
-  },
+require("lazy").setup({
+	-- メインで使用してるテーマ
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd("colorscheme nightfox")
+			-- vim.cmd('colorscheme carbonfox')
+		end,
+		-- init is called during startup. Configuration for vim plugins typically should be set in an init function
+		init = function()
+			require("nightfox").setup({
+				groups = {
+					nightfox = theme.theme_override_settings("nightfox"),
+					carbonfox = theme.theme_override_settings("carbonfox"),
+				},
+			})
+		end,
+	},
 
-  -- ウィンドウサイズの変更を簡単にできるようにする
-  { 'simeji/winresizer', cmd = 'WinResizerStartResize' },
+	-- ウィンドウサイズの変更を簡単にできるようにする
+	{ "simeji/winresizer", cmd = "WinResizerStartResize" },
 
-  --  エメット
-  { 'mattn/emmet-vim', ft = { 'html', 'javascriptreact', 'typescriptreact', 'vue', 'php', 'ejs', 'svelte' } },
+	--  エメット
+	{ "mattn/emmet-vim", ft = { "html", "javascriptreact", "typescriptreact", "vue", "php", "ejs", "svelte" } },
 
-  -- tree-sitterを用いたコードのシンタックスハイライトを行うプラグイン
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+	-- tree-sitterを用いたコードのシンタックスハイライトを行うプラグイン
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-  -- vim helpを日本語化
-  'vim-jp/vimdoc-ja', cmd = 'help',
+	-- vim helpを日本語化
+	{
+		"vim-jp/vimdoc-ja",
+		lazy = true,
+		-- コマンドモードで "h" が入力された時のみ読み込む
+		keys = {
+			{ "h", mode = "c" },
+		},
+	},
 
-  -- 各種Lintを非同期実行
-  'w0rp/ale',
+	-- 各種Lintを非同期実行
+	"w0rp/ale",
 
-  -- コメントアウトを効率化
-  'terrortylor/nvim-comment',
+	-- コメントアウトを効率化
+	{
+		"terrortylor/nvim-comment",
+		event = "VeryLazy",
+	},
 
-  -- ホバーしてる単語をカレントディレクトリ内から検索
-  { 'pechorin/any-jump.vim', cmd = 'AnyJump' },
+	-- ホバーしてる単語をカレントディレクトリ内から検索
+	{ "pechorin/any-jump.vim", cmd = "AnyJump" },
 
-  -- ripgrepをnvim上で実行して、検索結果をQuickfixに表示
-  { 'duane9/nvim-rg', cmd = 'Rg' },
+	-- ripgrepをnvim上で実行して、検索結果をQuickfixに表示
+	{ "duane9/nvim-rg", cmd = "Rg" },
 
-  -- 複数ファイルの一括置換
-  { 'thinca/vim-qfreplace', cmd = 'Qfreplace' },
+	-- 複数ファイルの一括置換
+	{ "thinca/vim-qfreplace", cmd = "Qfreplace" },
 
-  -- プロジェクトルートをカレントディレクトリにする
-  'mattn/vim-findroot',
+	-- プロジェクトルートをカレントディレクトリにする
+	"mattn/vim-findroot",
 
-  -- アイコンフォント
-  'ryanoasis/vim-devicons',
+	-- アイコンフォント
+	"ryanoasis/vim-devicons",
 
-  -- ファイラー
-  {
-    'lambdalisue/fern.vim',
-    dependencies = {
-      'lambdalisue/fern-renderer-devicons.vim',
-      'lambdalisue/glyph-palette.vim',
-      'lambdalisue/fern-git-status.vim',
-    },
-  },
+	-- ファイラー
+	{
+		"lambdalisue/fern.vim",
+		dependencies = {
+			"lambdalisue/fern-renderer-devicons.vim",
+			"lambdalisue/glyph-palette.vim",
+			"lambdalisue/fern-git-status.vim",
+		},
+	},
 
-  -- lsp
-  'neovim/nvim-lspconfig',
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
-  'hrsh7th/vim-vsnip',
-  {
-    'hrsh7th/nvim-cmp',
-    -- load cmp on InsertEnter
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-    },
-  },
+	-- lsp
+	{
+		"neovim/nvim-lspconfig",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		cmd = {
+			"Mason",
+			"MasonInstall",
+			"MasonUninstall",
+			"MasonUninstallAll",
+			"MasonLog",
+			"MasonUpdate",
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+	},
+	"hrsh7th/vim-vsnip",
+	{
+		"hrsh7th/nvim-cmp",
+		-- load cmp on InsertEnter
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
 
-  -- syntax for astro
-  { 'elel-dev/vim-astro-syntax', ft = { 'astro' } },
+	-- syntax for astro
+	{ "elel-dev/vim-astro-syntax", ft = { "astro" } },
 
-  -- ステータスライン
-  'nvim-lualine/lualine.nvim',
+	-- ステータスライン
+	"nvim-lualine/lualine.nvim",
 
-  -- Git操作
-  'dinhhuy258/git.nvim',
+	-- Git操作
+	"dinhhuy258/git.nvim",
 
-  -- fuzzy finder 
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  },
+	-- fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-  -- カラーコードの色を見えやすいように表示
-  {
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end,
-  },
+	-- カラーコードの色を見えやすいように表示
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 
-  -- increment/decrementを拡張する
-  'monaqa/dial.nvim',
+	-- increment/decrementを拡張する
+	"monaqa/dial.nvim",
 })
