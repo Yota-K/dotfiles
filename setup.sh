2bc3afd2c534c937686b9cfe3c618bab6fee31bb
Y.K <powdersugar828828@gmail.com>
2024-02-16 17:19:51 +0900

    Add comment to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 DOTFILES_DIR="$HOME/dotfiles"
 
-echo "シンボリックリンクを設定します"
+echo "シンボリックリンクを設定します 📝"
 if cd "$DOTFILES_DIR"; then
   # cd でdotfilesディレクトリに移動できた場合、gitディレクトリと.DS_Storeファイルを除外し、隠しファイルを検索する
   # cut で3バイト目以降を取得し、リンク先のファイルパスを取得する
@@ -23,24 +23,33 @@ else
  echo "$DOTFILES_DIR が存在しません。"
 fi
 
-echo "cspellの設定をします"
-# vimのスペルチェック用の辞書をダウンロードして、cspellの設定ファイルを作成する
-mkdir -p ~/.local/share/cspell
-if [ ! -f ~/.local/share/cspell/vim.txt.gz ]; then
-  vim_dictionary_url="https://github.com/iamcco/coc-spell-checker/raw/master/dicts/vim/vim.txt.gz"
-  curl -fsSLo ~/.local/share/cspell/vim.txt.gz --create-dirs "$vim_dictionary_url"
-fi
-
-# cspell.jsonは隠しファイルではないため、ループではなく直接リンクを作成している
+# 隠しファイルではない設定ファイルにシンボリックリンクを設定する
 ln -sfv "$DOTFILES_DIR/cspell.json" "$HOME/cspell.json"
 
-# weztermの追加モジュールは、~/.config/wezterm/ に配置しないといけない
-# ~/.config/weztermディレクトリが存在しない場合は作成して、~/theme.luaからシンボリックリンクを設定してファイルを参照できるようにする
+# MEMO: theme.luaはneovimのテーマの設定も共通化するためのモジュールである。
+# .config/wezterm/にtheme.luaを配置すればこの処理は不要だが、.config/.weztermディレクトリにあるとweztermの設定を行う用途みたいな感じで違和感があるので、
+# dotfilesのトップレベルに配置している。
 #
-# MEMO: .config/wezterm/にtheme.luaを配置すればこの処理は不要だが、theme.luaはneovimのテーマの設定も共通化するためのモジュールである。
-# .config/.weztermディレクトリにあるとweztermの設定を行うファイルみたいな感じで違和感があるので、dotfilesのトップレベルに配置している。
+# theme.luaはweztermの追加モジュールとしての役割も兼ねているので、~/.config/wezterm/ に配置している。
+# https://wezfurlong.org/wezterm/config/files.html#making-your-own-lua-modules
 mkdir -p ~/.config/wezterm
 
 if [ -d "$HOME/.config/wezterm" ]; then
   ln -sfv "$DOTFILES_DIR/theme.lua" "$HOME/.config/wezterm/theme.lua"
 fi
+echo "シンボリックリンクの設定が完了しました ✅"
+echo ""
+
+echo "cspellの設定をします 📝"
+# vimのスペルチェック用の辞書をダウンロードして、cspellの設定ファイルを作成する
+mkdir -p ~/.local/share/cspell
+if [ ! -f ~/.local/share/cspell/vim.txt.gz ]; then
+  vim_dictionary_url="https://github.com/iamcco/coc-spell-checker/raw/master/dicts/vim/vim.txt.gz"
+  # -f: サーバーエラーでサイレントに失敗する
+  # -s: プログレスメーターやエラーメッセージを表示しない
+  # -S: -sとともに使用すると、失敗した場合にcurlにエラーメッセージが表示される
+  # -L: リダイレクトを追う。アクセスしたページが移動していた場合、もう１度移動先にリクエストを送る。
+  # -o: ダウンロードしたファイルを保存する
+  curl -fsSLo ~/.local/share/cspell/vim.txt.gz --create-dirs "$vim_dictionary_url"
+fi
+echo "cspellの設定が完了しました ✅"
