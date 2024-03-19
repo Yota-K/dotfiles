dda14554ddff93187841f6a7c67eb7b8d4160886
Y.K <powdersugar828828@gmail.com>
2024-03-19 21:58:43 +0900

    [nvim] Refactor glow command.

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -28,14 +28,26 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 })
 
 -- Glowをvim上で実行する
+-- TODO: 動作が安定しないの
 nvim_create_user_command("Glow", function()
-  vim.cmd([[
-  if !executable('glow')
-    call s:echo_err("not found glow command.")
-  endif
+  if not vim.fn.executable("glow") then
+    vim.notify("Not found glow command.", vim.log.levels.ERROR)
+    return
+  end
 
-  execute('vs | wincmd j | resize 100 | terminal glow')
-]])
+  -- markdownファイル以外で実行した場合はエラーを出力
+  if not string.match(vim.fn.expand("%:p"), "%.md$") then
+    vim.notify("This is not a markdown file.", vim.log.levels.ERROR)
+    return
+  end
+
+  vim.cmd([[
+    execute('vs | wincmd j | resize 100 | terminal glow')
+    " TermClose の時点で現在フォーカスされている無関係なバッファに bdelete を送信することを回避し、
+    " 代わりに TermClose をトリガーした実際のバッファをターゲットすることでエラーを回避する
+    " https://github.com/neovim/neovim/issues/14986#issuecomment-902705190
+    autocmd TermClose * execute 'bdelete! ' . expand('<abuf>')
+  ]])
 end, {})
 
 -- タブラインのカスタマイズ
