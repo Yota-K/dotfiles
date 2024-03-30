3a603eec14da2de2fb7fdba2a4ec3845c8f8b7ba
Y.K <powdersugar828828@gmail.com>
2024-03-31 00:08:23 +0900

    [nvim] Refactor fern settings to lua.

--- .config/nvim/lua/plugin_settings/fern.lua
+++ .config/nvim/lua/plugin_settings/fern.lua
@@ -8,51 +8,54 @@ return {
     "lambdalisue/glyph-palette.vim",
     "lambdalisue/fern-git-status.vim",
   },
-  config = function()
+  init = function()
     vim.api.nvim_set_keymap("n", "<C-n>", ":Fern . -reveal=% -drawer -toggle<CR>", { noremap = true, silent = true })
+  end,
+  config = function()
+    vim.g["fern#renderer"] = "nerdfont"
+    vim.g["fern#default_hidden"] = 1
+    vim.g["fern#drawer_width"] = 40
+    vim.g["fern#fern_disable_startup_warnings"] = 1
+
+    local init_fern = function()
+      local keymap_options = { noremap = false, buffer = true }
+
+      vim.keymap.set("n", "i", "<Plug>(fern-action-open:split)", keymap_options)
+      vim.keymap.set("n", "s", "<Plug>(fern-action-open:vsplit)", keymap_options)
+
+      vim.keymap.set("n", "b", "<Plug>(fern-action-new-file)", keymap_options)
+      vim.keymap.set("n", "B", "<Plug>(fern-action-new-dir)", keymap_options)
+      vim.keymap.set("n", "r", "<Plug>(fern-action-rename)", keymap_options)
+
+      vim.keymap.set("n", "d", "<Plug>(fern-action-trash)", keymap_options)
+      vim.keymap.set("n", "D", "<Plug>(fern-action-remove)", keymap_options)
+
+      vim.keymap.set("n", "dd", "<Plug>(fern-action-move)", keymap_options)
+      vim.keymap.set("n", "y", "<Plug>(fern-action-clipboard-copy)", keymap_options)
+      vim.keymap.set("n", "p", "<Plug>(fern-action-clipboard-paste)", keymap_options)
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "fern",
+      callback = init_fern,
+    })
+
+    -- アイコンに色をつける
+    vim.api.nvim_create_augroup("my-glyph-palette", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "fern",
+      callback = function()
+        vim.cmd("call glyph_palette#apply()")
+      end,
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "nerdtree", "startify" },
+      callback = function()
+        vim.cmd("call glyph_palette#apply()")
+      end,
+    })
 
     vim.cmd([[
-      let g:fern#renderer = "nerdfont"
-      let g:fern#default_hidden = 1
-      let g:fern#drawer_width = 40
-      let g:fern_disable_startup_warnings = 1
-
-      function! s:init_fern() abort
-        " Write custom code here
-
-        nmap <buffer> i <Plug>(fern-action-open:split)
-        nmap <buffer> s <Plug>(fern-action-open:vsplit)
-
-        nmap <buffer> b <Plug>(fern-action-new-file)
-        nmap <buffer> B <Plug>(fern-action-new-dir)
-        nmap <buffer> r <Plug>(fern-action-rename)
-
-        nmap <buffer> d <Plug>(fern-action-trash)
-        nmap <buffer> D <Plug>(fern-action-remove)
-
-        nmap <buffer> dd <Plug>(fern-action-move)
-        nmap <buffer> y <Plug>(fern-action-clipboard-copy)
-        nmap <buffer> p <Plug>(fern-action-clipboard-paste)
-
-        nmap <buffer> R
-          \ <Plug>(fern-action-reload)
-          \ <Plug>(fern-action-reload:cursor)
-
-        nmap <buffer> / <Plug>(fern-action-grep=)
-      endfunction
-
-      augroup fern-custom
-        autocmd! *
-        autocmd FileType fern call s:init_fern()
-      augroup END
-
-      " アイコンに色をつける
-      augroup my-glyph-palette
-        autocmd! *
-        autocmd FileType fern call glyph_palette#apply()
-        autocmd FileType nerdtree,startify call glyph_palette#apply()
-      augroup END
-
       " Error detected while processing function <lambda>1072[1]..<SNR>123_show[52]..<SNR>123_apply:
       " line    4:
       " E28: No such highlight group name: FernRootSymbolの解消
