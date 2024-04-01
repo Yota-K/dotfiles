f5552fb476f8f3c1c3221d17a2c2eb9e87c703b9
Y.K <powdersugar828828@gmail.com>
2024-04-02 01:46:44 +0900

    [nvim] Update any-jump settings.

--- .config/nvim/lua/plugin_settings/any-jump.lua
+++ .config/nvim/lua/plugin_settings/any-jump.lua
@@ -3,11 +3,26 @@ return {
   "pechorin/any-jump.vim",
   cmd = "AnyJump",
   config = function()
-    vim.cmd([[
-      " agではなく、ripgrepを使用する
-      let g:any_jump_search_prefered_engine = 'rg'
-      " Custom ignore files
-      let g:any_jump_ignored_files = ['log', 'logs', 'images', 'imgs', 'img', 'node_modules', 'out', 'build', 'vendor', '.git', 'jquery-*.js', '*.bak', '*.map', '*.tmp', '*.temp']
-    ]])
+    -- agではなく、ripgrepを使用する
+    vim.g["any_jump_search_prefered_engine"] = "rg"
+
+    -- Custom ignore files
+    vim.g["any_jump_ignored_files"] = {
+      "log",
+      "logs",
+      "images",
+      "imgs",
+      "img",
+      "node_modules",
+      "out",
+      "build",
+      "vendor",
+      ".git",
+      "jquery-*.js",
+      "*.bak",
+      "*.map",
+      "*.tmp",
+      "*.temp",
+    }
   end,
 }
