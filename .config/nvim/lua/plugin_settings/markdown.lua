48aa6b104e348c18bc9b85f7121a9a6b6daec858
Y.K <powdersugar828828@gmail.com>
2024-06-09 20:54:47 +0900

    [nvim] Add keymap of markdown preview plugin.

--- .config/nvim/lua/plugin_settings/markdown.lua
+++ .config/nvim/lua/plugin_settings/markdown.lua
@@ -1,13 +1,18 @@
 -- markdownを見やすくするプラグイン
 return {
   "MeanderingProgrammer/markdown.nvim",
-  ft = { "markdown" },
   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
   -- tree-sitterの以下のparserに依存している
   -- markdown
   -- markdown_inline
   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  cmd = "RenderMarkdownToggle",
+  ft = { "markdown" },
+  keys = {
+    { "<C-r>", mode = "n" },
+  },
   config = function()
+    vim.api.nvim_set_keymap("n", "<C-r>", "<cmd>RenderMarkdownToggle<CR>", { noremap = true, silent = true })
     require("render-markdown").setup({})
   end,
 }
