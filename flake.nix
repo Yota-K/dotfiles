bda6ca11248a9257a20c61b5996d854d15117a8a
Y.K <powdersugar828828@gmail.com>
2024-11-23 14:17:01 +0900

    [halfway] Brew -> nix

--- flake.nix
+++ flake.nix
@@ -9,9 +9,23 @@
     packages.x86_64-darwin.my-packages = nixpkgs.legacyPackages.x86_64-darwin.buildEnv {
       name = "my-packages";
       paths = [
-        nixpkgs.legacyPackages.x86_64-darwin.git
+        # エラーで落ちたのでコメントアウト
+        # nixpkgs.legacyPackages.x86_64-darwin.aws-sam-cli
+        nixpkgs.legacyPackages.x86_64-darwin.alacritty
+        nixpkgs.legacyPackages.x86_64-darwin.bat
+        # エラーで落ちたのでコメントアウト
+        # nixpkgs.legacyPackages.x86_64-darwin.deno
         nixpkgs.legacyPackages.x86_64-darwin.curl
-        # ここにパッケージを追記していく
+        nixpkgs.legacyPackages.x86_64-darwin.direnv
+        nixpkgs.legacyPackages.x86_64-darwin.eza
+        nixpkgs.legacyPackages.x86_64-darwin.git
+        nixpkgs.legacyPackages.x86_64-darwin.jq
+        nixpkgs.legacyPackages.x86_64-darwin.lazygit
+        nixpkgs.legacyPackages.x86_64-darwin.ripgrep
+        nixpkgs.legacyPackages.x86_64-darwin.tig
+        nixpkgs.legacyPackages.x86_64-darwin.vim
+        nixpkgs.legacyPackages.x86_64-darwin.volta
+        nixpkgs.legacyPackages.x86_64-darwin.zellij
       ];
     };
   };
