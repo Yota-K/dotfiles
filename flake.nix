89486a7d0e82c03dc781443cede8747b397c6083
Y.K <powdersugar828828@gmail.com>
2024-11-27 01:15:13 +0900

    [nix] Multi-platform compatible.

--- flake.nix
+++ flake.nix
@@ -1,37 +1,44 @@
 {
-  description = "My package list for mac os.";
+  description = "My package list.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, neovim-nightly-overlay }: {
-    packages.x86_64-darwin.my-packages = nixpkgs.legacyPackages.x86_64-darwin.buildEnv {
-      name = "my-packages";
-      paths = [
-        # エラーで落ちたのでコメントアウト
-        # nixpkgs.legacyPackages.x86_64-darwin.aws-sam-cli
-        nixpkgs.legacyPackages.x86_64-darwin.alacritty
-        nixpkgs.legacyPackages.x86_64-darwin.bat
-        # エラーで落ちたのでコメントアウト
-        # nixpkgs.legacyPackages.x86_64-darwin.deno
-        nixpkgs.legacyPackages.x86_64-darwin.curl
-        nixpkgs.legacyPackages.x86_64-darwin.direnv
-        nixpkgs.legacyPackages.x86_64-darwin.eza
-        nixpkgs.legacyPackages.x86_64-darwin.git
-        nixpkgs.legacyPackages.x86_64-darwin.jq
-        nixpkgs.legacyPackages.x86_64-darwin.lazygit
-        nixpkgs.legacyPackages.x86_64-darwin.ripgrep
-        nixpkgs.legacyPackages.x86_64-darwin.tig
-        nixpkgs.legacyPackages.x86_64-darwin.tmux
-        nixpkgs.legacyPackages.x86_64-darwin.vim
-        nixpkgs.legacyPackages.x86_64-darwin.volta
-        nixpkgs.legacyPackages.x86_64-darwin.zellij
-
-        # NOTE: Overlaysという仕組みを利用することでNixpkgsを拡張・上書きすることができる
-        neovim-nightly-overlay.packages.x86_64-darwin.neovim
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, neovim-nightly-overlay, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    # NOTE: let-in構文を使うことで関数内で変数を宣言できる
+    let
+      # NOTE: Overlaysという仕組みを利用することでNixpkgsを拡張・上書きすることができる
+      pkgs = nixpkgs.legacyPackages.${system}.extend (neovim-nightly-overlay.overlays.default);
+    in
+    {
+      packages.my-packages = pkgs.buildEnv {
+        name = "my-packages";
+        paths = with pkgs; [
+          # エラーで落ちたのでコメントアウト
+          # nixpkgs.legacyPackages.${system}.aws-sam-cli
+          alacritty
+          bat
+          # エラーで落ちたのでコメントアウト
+          # nixpkgs.legacyPackages.${system}.deno
+          curl
+          direnv
+          eza
+          git
+          jq
+          lazygit
+          ripgrep
+          tig
+          tmux
+          vim
+          volta
+          zellij
+          neovim
+        ];
+      };
+    }
+  );
 }
