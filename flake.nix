6e7eed00b5e1d2747ab926fc7f1e8627900a3ad7
Y.K <powdersugar828828@gmail.com>
2024-11-27 01:01:21 +0900

    [halfway] Brew -> nix

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, neovim-nightly-overlay }: {
     packages.x86_64-darwin.my-packages = nixpkgs.legacyPackages.x86_64-darwin.buildEnv {
       name = "my-packages";
       paths = [
@@ -23,9 +24,13 @@
         nixpkgs.legacyPackages.x86_64-darwin.lazygit
         nixpkgs.legacyPackages.x86_64-darwin.ripgrep
         nixpkgs.legacyPackages.x86_64-darwin.tig
+        nixpkgs.legacyPackages.x86_64-darwin.tmux
         nixpkgs.legacyPackages.x86_64-darwin.vim
         nixpkgs.legacyPackages.x86_64-darwin.volta
         nixpkgs.legacyPackages.x86_64-darwin.zellij
+
+        # NOTE: Overlaysという仕組みを利用することでNixpkgsを拡張・上書きすることができる
+        neovim-nightly-overlay.packages.x86_64-darwin.neovim
       ];
     };
   };
