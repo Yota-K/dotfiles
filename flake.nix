acfd450dbd5dae9b66973608afb8450b129cc70c
Y.K <powdersugar828828@gmail.com>
2024-12-04 23:37:43 +0900

    [nix] add package for code formatter.

--- flake.nix
+++ flake.nix
@@ -10,69 +10,82 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, neovim-nightly-overlay, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    # NOTE: let-in構文を使うことで関数内で変数を宣言できる
-    let
-      pkgs = import nixpkgs {
-        system = system;
-        # Unfreeパッケージをインストールできるようにする
-        # ref: https://nixos.org/manual/nixpkgs/stable/#sec-allow-unfree
-        config.allowUnfree = true;
-        overlays = [ neovim-nightly-overlay.overlays.default ];
-      };
-    in
+  outputs =
     {
-      allowUnfree = true;
+      self,
+      nixpkgs,
+      neovim-nightly-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      # NOTE: let-in構文を使うことで関数内で変数を宣言できる
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          # Unfreeパッケージをインストールできるようにする
+          # ref: https://nixos.org/manual/nixpkgs/stable/#sec-allow-unfree
+          config.allowUnfree = true;
+          overlays = [ neovim-nightly-overlay.overlays.default ];
+        };
+      in
+      {
+        allowUnfree = true;
 
-      packages.my-packages = pkgs.buildEnv {
-        name = "my-packages";
-        paths = with pkgs; [
-          alacritty
-          bat
-          curl
-          deno
-          direnv
-          eza
-          git
-          gitflow
-          graphviz
-          jq
-          lazygit
-          libffi
-          libssh2
-          libzip
-          neovim
-          ngrok
-          rbenv
-          ripgrep
-          starship
-          tig
-          tmux
-          unbound
-          vim
-          vips
-          volta
-          zellij
-        ];
-      };
+        packages.my-packages = pkgs.buildEnv {
+          name = "my-packages";
+          paths = with pkgs; [
+            alacritty
+            bat
+            curl
+            deno
+            direnv
+            eza
+            git
+            gitflow
+            graphviz
+            jq
+            lazygit
+            libffi
+            libssh2
+            libzip
+            neovim
+            ngrok
+            nixfmt-rfc-style
+            rbenv
+            ripgrep
+            starship
+            tig
+            tmux
+            unbound
+            vim
+            vips
+            volta
+            zellij
+          ];
+        };
 
-      # nix run .#updateで以下のスクリプトを実行する
-      # - nix flake update: flakeのlockファイルを更新する
-      # - nix profile upgrade my-packages: 最新のフレークを使用してパッケージをアップグレードする
-      apps.update = {
-        type = "app";
-        program = toString (
-          pkgs.writeShellScript "update-script" ''
-            set -e
-            echo "Updating flake..."
-            nix flake update
-            echo "Updating profile..."
-            nix profile upgrade my-packages
-            echo "Update complete!"
-          ''
-        );
-      };
-    }
-  );
+        # nix run .#updateで以下のスクリプトを実行する
+        # - nix flake update: flakeのlockファイルを更新する
+        # - nix profile upgrade my-packages: 最新のフレークを使用してパッケージをアップグレードする
+        apps.update = {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript "update-script" ''
+              set -e
+              echo "Updating flake..."
+              nix flake update
+              echo "Updating profile..."
+              nix profile upgrade my-packages
+              echo "Update complete!"
+            ''
+          );
+        };
+
+        #  nixfmtを使用して、formatter実行できるようにする
+        # nix fmt でフォーマッターを実行できる
+        # https://github.com/NixOS/nixfmt
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
