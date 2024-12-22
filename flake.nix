c5feadc7a2571bcc12c1bb6c76b31bd2e4f53eb0
Y.K <powdersugar828828@gmail.com>
2024-12-22 17:04:08 +0900

    [nix] remove flake-utils.

--- flake.nix
+++ flake.nix
@@ -1,96 +1,91 @@
 {
   description = "My package list.";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    # Flake関連のユーティリティ関数
-    # マルチプラットフォーム対応を行うために使用している。
-    # https://github.com/numtide/flake-utils?tab=readme-ov-file#system---system--system--
-    flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs =
+    { self, nixpkgs, neovim-nightly-overlay }:
+    let
+      supportSystems = [
+        "x86_64-darwin" # macOS (64-bit x86)
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportSystems;
+    in
     {
-      self,
-      nixpkgs,
-      neovim-nightly-overlay,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      # NOTE: let-in構文を使うことで関数内で変数を宣言できる
-      let
-        pkgs = import nixpkgs {
-          system = system;
-          # Unfreeパッケージをインストールできるようにする
-          # ref: https://nixos.org/manual/nixpkgs/stable/#sec-allow-unfree
-          config.allowUnfree = true;
-          overlays = [ neovim-nightly-overlay.overlays.default ];
-        };
-      in
-      {
-        allowUnfree = true;
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            system = system;
+            config.allowUnfree = true; # Unfreeパッケージを許可
+            overlays = [ neovim-nightly-overlay.overlays.default ];
+          };
+        in
+        {
+          allowUnfree = true;
 
-        packages.my-packages = pkgs.buildEnv {
-          name = "my-packages";
-          paths = with pkgs; [
-            alacritty
-            bat
-            curl
-            deno
-            direnv
-            eza
-            git
-            gitflow
-            go
-            graphviz
-            jq
-            lazygit
-            libffi
-            libssh2
-            libzip
-            # luaのlatest versionは2024年12月現在だと5.4系だが、nixでインストールすると5.2系がインストールされる
-            # ref: https://github.com/NixOS/nixpkgs/issues/323083
-            lua
-            neovim
-            ngrok
-            nixfmt-rfc-style
-            perl
-            php
-            ripgrep
-            rustup
-            starship
-            tig
-            tmux
-            vim
-            vips
-            volta
-            zellij
-          ];
-        };
+          my-packages = pkgs.buildEnv {
+            name = "my-packages";
+            paths = with pkgs; [
+              alacritty
+              bat
+              curl
+              deno
+              direnv
+              eza
+              git
+              gitflow
+              go
+              graphviz
+              jq
+              lazygit
+              libffi
+              libssh2
+              libzip
+              lua
+              neovim
+              ngrok
+              nixfmt-rfc-style
+              perl
+              php
+              ripgrep
+              rustup
+              starship
+              tig
+              tmux
+              vim
+              vips
+              volta
+              zellij
+            ];
+          };
+        }
+      );
 
-        # nix run .#updateで以下のスクリプトを実行する
-        # - nix flake update: flakeのlockファイルを更新する
-        # - nix profile upgrade my-packages: 最新のフレークを使用してパッケージをアップグレードする
-        apps.update = {
-          type = "app";
-          program = toString (
-            pkgs.writeShellScript "update-script" ''
+      apps = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            system = system;
+          };
+        in
+        {
+          update = {
+            type = "app";
+            program = toString (pkgs.writeShellScript "update-script" ''
               set -e
               echo "Updating flake..."
               nix flake update
               echo "Updating profile..."
               nix profile upgrade my-packages
               echo "Update complete!"
-            ''
-          );
-        };
+            '');
+          };
+        }
+      );
 
-        #  nixfmtを使用して、formatter実行できるようにする
-        # nix fmt でフォーマッターを実行できる
-        # https://github.com/NixOS/nixfmt
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+    };
 }
