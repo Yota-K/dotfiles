caa15b684638ba353b6041804aaf5c8621edf89b
Y.K <powdersugar828828@gmail.com>
2025-01-05 02:02:27 +0900

    brew -> nix 🎉

--- .config/nix/nix-darwin/default.nix
+++ .config/nix/nix-darwin/default.nix
@@ -1,20 +1,64 @@
-# in configuration.nix
 { pkgs, ... }: {
-  system.stateVersion = 4;
+  fonts = {
+    packages = with pkgs; [
+      hack-font
+      hackgen-font
+      hackgen-nf-font
+    ];
+  };
 
   system = {
+    # 下位互換性のため
+    # ないとエラーで落ちた
+    stateVersion = 4;
+
+    # https://daiderd.com/nix-darwin/manual/index.html
     defaults = {
+      # Finder ですべてのファイル拡張子を表示するかどうか
       NSGlobalDomain.AppleShowAllExtensions = true;
       finder = {
+        # 隠しファイルを常に表示するかどうか。
         AppleShowAllFiles = true;
+        # ファイル拡張子を常に表示するかどうか
         AppleShowAllExtensions = true;
       };
       dock = {
+        # ドックを自動的に非表示または表示するかどうか。
         autohide = true;
+        # ドックに最近使用したアプリケーションを表示します
         show-recents = false;
+        # メニューバーの表示位置
         orientation = "left";
       };
     };
   };
+
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+    };
+    brews = [
+      "glib"
+      "cairo"
+      "gobject-introspection"
+      "libffi"
+      "libpq"
+      "poppler"
+      "postgresql@14"
+      "ruby-build"
+    ];
+    casks = [
+      "arc"
+      "figma"
+      "google-chrome"
+      "raycast"
+      "sequel-ace"
+      "wezterm@nightly"
+      "zoom"
+    ];
+    taps = [
+      "homebrew/bundle"
+    ];
+  };
 }
-# inputs.self, inputs.nix-darwin, and inputs.nixpkgs can be accessed here
