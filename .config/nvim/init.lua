833513ef51a266dc21661cded617781bd85b4c0e
Y.K <powdersugar828828@gmail.com>
2026-01-04 16:13:25 +0900

    [nvim] tabline function move from functions.lua.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,15 +14,8 @@ require("base")
 require("functions")
 require("keymap")
 require("plugins")
+require("tabline")
 require("terminal")
-
--- デフォルトテーマを使う時はここにかく
--- local plugin_module_path = ";" .. os.getenv("HOME") .. "/dotfiles/.config/nvim/plugin_settings/?.lua;"
--- package.path = package.path .. plugin_module_path
--- local theme = require("theme")
---
--- theme.theme_override_settings()
--- vim.cmd("colorscheme retrobox")
 
 -- フローティングウィンドウを透明にする
 vim.cmd([[
