049cc6b73e298fcda3af9a5c1be969713aca2998
Y.K <powdersugar828828@gmail.com>
2026-01-11 16:22:22 +0900

    [nix]: refactor

--- .config/nix/home-manager/home.nix
+++ .config/nix/home-manager/home.nix
@@ -4,60 +4,85 @@
   ...
 }:
 let
-  tools = with pkgs; [
-    # aws-sam-cli
-    aws-vault
-    awscli2
+  # CLI/TUI系
+  cliTools = with pkgs; [
     bat
     claude-code
     curl
-    ctop
     delta
-    deno
     direnv
-    ecspresso
     eza
     fish
     fzf
-    gh
-    git
-    gitflow
-    go
-    google-cloud-sdk
-    google-cloud-sql-proxy
-    graphviz
     jq
-    lazygit
-    libssh2
-    libzip
-    lua
-    minikube
-    ngrok
-    nixfmt-rfc-style
-    orbstack
-    perl
-    php
-    pyenv
     ripgrep
-    rustup
     starship
-    stylua
     tenv
-    uv
     vim-startuptime
-    vips
-    volta
     yazi
   ];
+
+  # パブリッククラウド/コンテナ関連
+  cloudTools = with pkgs; [
+    aws-vault
+    awscli2
+    ctop
+    ecspresso
+    google-cloud-sdk
+    google-cloud-sql-proxy
+    minikube
+    ngrok
+    orbstack
+  ];
+
+  # Editors
   editors = with pkgs; [
     vim
     inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
   ];
-  terminalPackages = with pkgs; [
+
+  # コードフォーマッタ
+  formatters = with pkgs; [
+    nixfmt-rfc-style
+    stylua
+  ];
+
+  # Git関連ツール
+  gitTools = with pkgs; [
+    gh
+    git
+    gitflow
+    lazygit
+  ];
+
+  # プログラミング言語/ランタイム/バージョン管理/パッケージマネージャ
+  languages = with pkgs; [
+    deno
+    go
+    lua
+    perl
+    php
+    pyenv
+    rustup
+    uv
+    volta
+  ];
+
+  # その他
+  others = with pkgs; [
+    graphviz
+    libssh2
+    libzip
+    vips
+  ];
+
+  # ターミナル関連
+  terminals = with pkgs; [
     alacritty
     tmux
     zellij
   ];
+
   username = "public";
 in
 {
@@ -87,7 +112,8 @@ in
     homeDirectory = "/Users/${username}";
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     stateVersion = "24.05";
-    packages = tools ++ editors ++ terminalPackages;
+    packages =
+      cliTools ++ cloudTools ++ editors ++ formatters ++ gitTools ++ languages ++ others ++ terminals;
   };
 
   programs.home-manager = {
