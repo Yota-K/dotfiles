1f5eabd0170f590f4d1b22da8e25f486efd0a948
Y.K <powdersugar828828@gmail.com>
2026-05-15 02:07:53 +0900

    refactor(nvim): migrate vim.cmd highlights to Lua API

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -51,7 +51,12 @@ opt.laststatus = 3
 -- エラーやヒントがある時は行数の部分を上書きして表示する
 opt.signcolumn = "number"
 
--- ログの保存先をtmpに変更
+-- ターミナルでも True Color を使えるようにする
+opt.termguicolors = true
+-- ポップアップメニューに透過を付与する
+opt.pumblend = 20
+
+-- ログの肥大化を防止するため、ログの保存先をtmpに変更
 vim.env.XDG_STATE_HOME = '/tmp'
 
 -- マウス操作を無効化
@@ -66,19 +71,35 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   end,
 })
 
-vim.cmd([[
-  " 使用しているテーマ関係なく必ず有効にしたい配色の設定
-  augroup highlightIdegraphicSpace
-    autocmd!
-    autocmd Colorscheme * highlight IdeographicSpace term=underline ctermbg=DarkGreen guibg=DarkGreen
-    autocmd VimEnter,WinEnter * match IdeographicSpace /　/
-  augroup END
+-- 使用しているテーマ関係なく必ず有効にしたい配色の設定
+local ideographic_space = vim.api.nvim_create_augroup("highlightIdeographicSpace", { clear = true })
 
-  autocmd ColorScheme * highlight StatusLine NONE
-  autocmd ColorScheme * highlight TabLineFill ctermbg=NONE guibg=NONE
-  autocmd ColorScheme * highlight SignColumn NONE
+-- 全角スペースをハイライト表示するためのカラー定義
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = ideographic_space,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "IdeographicSpace", {
+      underline = true,
+      ctermbg = "DarkGreen",
+      bg = "DarkGreen",
+    })
+  end,
+})
 
-  " ターミナルでも True Color を使えるようにする。
-  set termguicolors
-  set pumblend=20
-]])
+-- ウィンドウを開いた時に全角スペースのマッチを有効化
+vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter" }, {
+  group = ideographic_space,
+  pattern = "*",
+  command = [[match IdeographicSpace /　/]],
+})
+
+-- StatusLine / TabLineFill / SignColumn の背景をテーマに依存させず透過させる
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "StatusLine", {})
+    vim.api.nvim_set_hl(0, "TabLineFill", { ctermbg = "NONE", bg = "NONE" })
+    vim.api.nvim_set_hl(0, "SignColumn", {})
+  end,
+})
