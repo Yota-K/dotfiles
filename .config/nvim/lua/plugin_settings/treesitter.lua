d91a911e37d2e1908f3402dcf5e0fea15e62fe4d
Y.K <powdersugar828828@gmail.com>
2026-05-17 21:43:25 +0900

    style(nvim): sort treesitter ensure_installed alphabetically

--- .config/nvim/lua/plugin_settings/treesitter.lua
+++ .config/nvim/lua/plugin_settings/treesitter.lua
@@ -20,33 +20,34 @@ return {
     })
 
     local ensure_installed = {
+      "astro",
       "css",
       "fish",
       "go",
+      "graphql",
       "html",
       "javascript",
       "json",
       "lua",
+      "markdown",
+      "markdown_inline",
+      "nix",
       "php",
+      "prisma",
       "scss",
+      "svelte",
       "tsx",
       "typescript",
       "vim",
       "vue",
       "yaml",
-      "graphql",
-      "prisma",
-      "astro",
-      "markdown",
-      "markdown_inline",
-      "nix",
     }
     local already_installed = require("nvim-treesitter").get_installed()
     local parsers_to_install = vim.iter(ensure_installed)
-      :filter(function(parser)
-        return not vim.tbl_contains(already_installed, parser)
-      end)
-      :totable()
+        :filter(function(parser)
+          return not vim.tbl_contains(already_installed, parser)
+        end)
+        :totable()
     if #parsers_to_install > 0 then
       require("nvim-treesitter").install(parsers_to_install)
     end
