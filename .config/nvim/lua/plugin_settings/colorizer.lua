e2e7af1ad50e73b85dbc3eded42b4792a2446686
Y.K <powdersugar828828@gmail.com>
2026-06-01 18:45:08 +0900

    fix(nvim): switch nvim-highlight-colors to fork temporarily
    Use Yota-K's fork until upstream PR for issue #179 is merged.

--- .config/nvim/lua/plugin_settings/colorizer.lua
+++ .config/nvim/lua/plugin_settings/colorizer.lua
@@ -1,6 +1,7 @@
 -- カラーコードの色を見えやすいように表示
 return {
-  "brenoprata10/nvim-highlight-colors",
+  "Yota-K/nvim-highlight-colors", -- TODO: PR (https://github.com/brenoprata10/nvim-highlight-colors/issues/179) マージ後に upstream に戻す
+  commit = "08407be4943fb5726bd93c0a8cf36243c9f60c89",
   event = {
     "BufReadPre",
     "BufNewFile",
